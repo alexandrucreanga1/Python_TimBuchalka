a211fc550f739b74e95fea9d18f73704f5059823
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-06 17:39:38 +0300

    hello World !

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,11 @@
 print('Hello World ! ')
 
 print ('Heloo there anywhere!')
+print('Ani bani richi fachi!')
+print(1+2)
+print(7*6)
+print()
+print('The End!')
+
+
+
---
+++ strings.py
@@ -0,0 +1,5 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are wasy to use")
+print('We can include "quotes" in strings')
+print("hello" + " world") # string concatenation !
