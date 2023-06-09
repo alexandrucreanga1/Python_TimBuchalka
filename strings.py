699fee5aa13f90e234dc49d971802d493d0a44cb
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-09 17:14:46 +0300

    hello World !

--- strings.py
+++ strings.py
@@ -9,3 +9,15 @@ greeting = "Hello"
 name = input("Please enter your name ")
 
 print(greeting+" " + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(age)
+print(type(age))
+print(name + " is " + str(age) + " years old.")  # strongly typed !
