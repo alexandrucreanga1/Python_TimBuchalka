de3f114158784987531b461b1160999233594c5f
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-12 23:52:50 +0300

    hello World !

--- strings2Slicing.py
+++ strings2Slicing.py
@@ -8,3 +8,29 @@ print(parrot[3:5]) # we
 
 print(parrot[0:9])
 print(parrot[:9])
+print(parrot[10:14])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+print(letters[0:6]) # Norweg
+print(letters[3:5]) # we
+
+print(letters[0:9])
+print(letters[:9])
+print(letters[10:14])
+print(letters[10:])
+
+print(letters[:6])
+print(letters[6:])
+
+print(letters[:6] + letters[6:])
+
+print(letters[:])
