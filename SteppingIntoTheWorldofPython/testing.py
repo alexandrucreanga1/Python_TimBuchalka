242caa54c70310c1b5780884bd261e94694e0018
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-13 18:10:11 +0300

    hello World !

---
+++ SteppingIntoTheWorldofPython/testing.py
@@ -0,0 +1,22 @@
+
+
+meal1 = "spam" + "eggs" + "beans"
+meal3 = "spam, eggs, beans"
+print(meal1)
+print(meal3)
+meal2 = "spam\neggs\nbeans"
+print(meal2)
+
+# name ="John"
+# age = 25
+# print(name+age)
+
+quantity = 10
+price = 5.0
+total = quantity * price
+tax = total / 5
+
+Total = total + tax
+
+print(total)
+print(total == Total)
