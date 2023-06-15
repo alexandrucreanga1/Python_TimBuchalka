66b8a3ee70564110852811e17a4b430009dbfee4
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-15 17:43:28 +0300

    hello World !

--- ProgramFlow/guessinggame.py
+++ ProgramFlow/guessinggame.py
@@ -8,9 +8,19 @@ guess = int(input())
 if guess < answer :
     print("Please guess higher !")
     print("Correct number is: " + str(answer))
+    guess = int(input())
+    if guess == answer:
+        print("well done, you guessed it")
+    else:
+        print("Sorry wrong number at this time")
 elif guess > answer :
     print("Please guess lower !")
     print("Correct number is: " + str(answer))
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("sorry, wrong number at this time")
 else :
     print("You got it first time !")
     print("Correct number is: " + str(answer))
