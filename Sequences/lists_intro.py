a04b9c19398d94c1e47ec1dd4711ac89df91d15a
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-27 17:45:10 +0300

    hello World !

--- Sequences/lists_intro.py
+++ Sequences/lists_intro.py
@@ -5,16 +5,24 @@ computer_parts = ["computer",
                   "mouse mat"
                   ]   # very simple example of list !!!!!!!!!!!
 
+#
+# print(computer_parts)
+# print("_________________________________________________")
+# for part in computer_parts:
+#     print(part)
+#
+# print()
+# print(computer_parts[2])
+#
+# print()
+# print(computer_parts[0:3])
+# print("___________")
+# print(computer_parts[-1])
 
 print(computer_parts)
-print("_________________________________________________")
-for part in computer_parts:
-    print(part)
+# computer_parts[3] = "trackball"
+print(computer_parts[3:])
 
-print()
-print(computer_parts[2])
+computer_parts[3:] = ["trackball"] #here be careful for square brackets !!!!!!!!!
+print(computer_parts)
 
-print()
-print(computer_parts[0:3])
-print("___________")
-print(computer_parts[-1])
--- Sequences/sorting.py
+++ Sequences/sorting.py
@@ -10,3 +10,17 @@ print(numbers)
 
 numbers.sort()
 print(numbers)
+
+missing_letter = sorted("The quick brown fox jumped over the lazy dog", key=str.casefold)
+print(missing_letter)
+
+names = ["Graham",
+         "John",
+         "terry",
+         "eric",
+         "Terry",
+         "michael"
+         ]
+
+names.sort(key=str.casefold)
+print(names)
