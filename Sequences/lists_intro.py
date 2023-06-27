860df2ba859913c3f96de88016a901d442c88b0e
alexandru.creanga1 <alexandru.creanga1@gmail.com>
2023-06-27 11:19:28 +0300

    hello World !

---
+++ Sequences/lists_intro.py
@@ -0,0 +1,20 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]   # very simple example of list !!!!!!!!!!!
+
+
+print(computer_parts)
+print("_________________________________________________")
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
+
+print()
+print(computer_parts[0:3])
+print("___________")
+print(computer_parts[-1])
